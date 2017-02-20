354f0bcdfb7ecceb27f7f216fe55ccab107f77b9
MyCloudSpace <virtual.itguy@gmail.com>
2017-02-20 19:31:19 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Welcome To My Cloud Space</title>
+    </head>
+
+    <body>
+<div align="center">
+                <h1>Hello My Cloud Space</h1>
+                <h2>WoW..! Congratulations! You have successfully installed an application to OpsWorks Instance!</h2>
+                <div align="center"><img src="https://s3-us-east-1.amazonaws.com/mycloudspace/logo.png"></div>
+                <p>Your MyCloudSpace Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
